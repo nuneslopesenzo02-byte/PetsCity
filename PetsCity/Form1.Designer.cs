8fc860293ec478cab9f9013395f21fc814aa38ce
enzo <nuneslopesenzo02@gmail.com>
2026-09-10 16:43:02 -0300

    Criando a classe do projeto

--- PetsCity/Form1.Designer.cs
+++ PetsCity/Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(293, 37);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(375, 37);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(568, 178);
+            button1.Name = "button1";
+            button1.Size = new Size(8, 61);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 615);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
             Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button button1;
     }
 }
---
+++ PetsCity/PetsCity.cs
@@ -0,0 +1,41 @@
+﻿
+namespace PetsCity
+{
+    class PetsCity
+    {
+        //ATRIBUTOS
+        public string? NomePet { get; set; }
+
+        public double PrecoServico { get; set; }
+
+        public int QuantidadeBanhosMensais { get; set; }
+
+        public int IdadeAnimal { get; set; }
+
+        //MÉTODOS
+
+        public double CalcularDescontoFidelidade()
+        {
+            double valorTotal = PrecoServico * QuantidadeBanhosMensais;
+
+            if (QuantidadeBanhosMensais >= 5)
+            {
+                return valorTotal * 0.10;
+            }
+            return 0.0;
+        }
+
+        public double ValorFinalPacote()
+        {
+            double ValorFinalBruto = PrecoServico * QuantidadeBanhosMensais;
+            double desconto = CalcularDescontoFidelidade();
+            return ValorFinalBruto - desconto;
+        }
+
+        public bool ValidarSeBrinde()
+        {
+            return (IdadeAnimal >= 8 && QuantidadeBanhosMensais >= 5) || (ValorFinalPacote() > 300.0);
+        }
+
+    }
+}
